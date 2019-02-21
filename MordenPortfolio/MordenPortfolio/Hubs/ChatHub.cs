5d63325186c7e0803fbe6b610459b2b789663587
Matt Morden <Mattymorden@gmail.com>
2019-02-20 21:32:34 -0500

    Add SignalR project

---
+++ MordenPortfolio/MordenPortfolio/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace MordenPortfolio.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message, string colour)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message, colour);
+        }
+    }
+}
--- MordenPortfolio/MordenPortfolio/Startup.cs
+++ MordenPortfolio/MordenPortfolio/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MordenPortfolio.Hubs;
 
 namespace MordenPortfolio
 {
@@ -33,6 +29,7 @@ namespace MordenPortfolio
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +48,10 @@ namespace MordenPortfolio
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
+            });
 
             app.UseMvc();
         }
